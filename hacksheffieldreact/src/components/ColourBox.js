935a3103e09665bc9fbc8141378ca9b111079bf1
Chris Hart <github@nxmee.com>
2020-11-07 15:59:24 +0000

    Case sensitivity is a bitch

--- hacksheffieldreact/src/components/ColourBox.js
+++ hacksheffieldreact/src/components/ColourBox.js
@@ -1,26 +1,50 @@
+//Represents a box representing a whole colour instance
 import React, { Component } from 'react'
 import PropTypes from 'prop-types';
+import ColourBlock from './ColourBlock';
 
 export class ColourBox extends Component {
-    getstyle = () => {
+    getouterstyle = () => {
         return {
-            backgroundColor: this.props.colour.colour, 
+            backgroundColor: '#222222', 
             display: 'flex',
             flexDirection: 'row',
-            padding:'10vh', 
+            height:'20vh',
+            width: '50%;',
+            margin: 'auto',
             justifyContent: 'flex-start',
             alignContent: 'stretch',
-            alignItems: 'flex-start',
+            alignItems: 'center',
             flex: '0 1 auto',
-            flexBasis: '100%'
+            flexBasis: '100%',
+            color:'#FFFFFF',
+            textAlign: 'center',
+        }
+    }
+    getinnerstyle = () => {
+        return {
+            backgroundColor: '#222222', 
+            height:'20vh',
+            width: '50%;',
+            margin: 'auto',
+            color:'#FFFFFF',
+            textAlign: 'center',
+            flex: '0 1 auto',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center'
         }
     }
 
     render() {
         return (
-            <div style={this.getstyle()}>
-                <p>{this.props.colour.location}</p>
+            <div style={this.getouterstyle()}>
+                <ColourBlock colour={this.props.colour}/>
+                <div style={this.getinnerstyle()}>
+                    <p>{this.props.colour.location}</p>
+                </div>
             </div>
+
         )
     }
 }
